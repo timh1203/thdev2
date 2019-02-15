fcc8eb907df0111090bac4c2d7457cb0410e7b76
Timothy Hoang <timh1203@gmail.com>
2019-02-15 05:28:15 -0600

    Update packages gatsby and sharp

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-images-contentful`,
             options: {
-              maxWidth: 650,
+              maxWidth: 600,
               linkImagesToOriginal: false,
             },
           },
