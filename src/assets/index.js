414d1dfa1c0d0d8bfc2d892f47057f65bdde754d
Timothy Hoang <timh1203@gmail.com>
2019-02-17 15:56:11 -0600

    Add remaining projects information

--- src/assets/index.js
+++ src/assets/index.js
@@ -261,17 +261,81 @@ export const projectsData = {
     github: 'https://github.com/timh1203/dullesBarber',
   },
   pomodoroApp: {
-    name: 'Pomodoro App',
+    name: 'Phenomenal Pomodoro App',
     desc:
       'The work/break cycle app based on the Pomodoro Technique by by Francesco Cirillo. The app was for the Free Code Camp curriculum. The technology used was javascript.',
     demo: 'https://timh1203.github.io/pomodoro-app/',
     github: 'https://github.com/timh1203/pomodoro-app',
   },
   calculatorApp: {
-    name: 'Calculator App',
+    name: 'Cool Calculator App',
     desc:
       'A simple calculator app to perform basic math operations for the Free Code Camp curriculum. The technology used was javascript.',
     demo: 'https://timh1203.github.io/calculator-app',
     github: 'https://github.com/timh1203/calculator-app',
+  },
+  derikJones: {
+    name: 'Derik Jones Portfolio Website',
+    desc:
+      'An architecture portfolio website. The technology stack includes GatsbyJS, GraphQL, and Styled Components.',
+    demo: 'http://derikjones.com/',
+    github: 'https://github.com/timh1203/derikjones',
+  },
+  respondezApp: {
+    name: 'Remarkable Respondez App',
+    desc:
+      'A custom event app built for the Free Code Camp Group. The technology stack includes ReactJS and FireBase.',
+    demo: 'https://timh1203.github.io/respondez-app/',
+    github: 'https://github.com/timh1203/respondez-app',
+  },
+  twitchApp: {
+    name: 'Terrific Twitch App',
+    desc:
+      'A Twitch app displaying the channels streaming status from the Twitch API. The technology stack includes ReactJS and FireBase.',
+    demo: 'https://timh1203.github.io/twitch-app/',
+    github: 'https://github.com/timh1203/twitch-app',
+  },
+  wikipediaApp: {
+    name: 'Witty Wikipedia App',
+    desc:
+      'A wikipedia app that displays a user search results or a random article from the WikiMedia API. The technology used was javascript.',
+    demo: 'https://timh1203.github.io/wikipedia-app/',
+    github: 'https://github.com/timh1203/wikipedia-app',
+  },
+  weatherApp: {
+    name: 'Wonderful Weather App',
+    desc:
+      "A weather app that displays the weather forecast (24 hours, 48 hours, and 1 week) based on the user's IP location. The technologies used were javascript, Dark Sky API, and Skycons.",
+    demo: 'https://timh1203.github.io/weather-app/',
+    github: 'https://github.com/timh1203/weather-app',
+  },
+  photographySite: {
+    name: 'Photography Website',
+    desc:
+      'A photography website that uses an API to pull pictures from instagram. The technologies used were javascript, Materialize, and InstafeedJS.',
+    demo: 'https://timh1203.github.io/photo-site/',
+    github: 'https://github.com/timh1203/photo-site',
+  },
+  turtleDash: {
+    name: 'Turtle Dash Chrome Extension',
+    desc:
+      'A clone of the popular "Momentum Dash" Chrome Extension. This was a remote effort of remote developers from around the world as part of the Free Code Camp\'s Chingu-Voyage Group. The technologies used were javascript and jQuery.',
+    demo:
+      'https://chrome.google.com/webstore/detail/turtle-dash/odijnkiclhddnfdgnbjghchkhagnooek',
+    github: 'https://github.com/chingu-voyage-turtles-2/turtle-dash',
+  },
+  quoteMachine: {
+    name: 'Random Quote Machine',
+    desc:
+      'A random quote generator with randoms quotes from an API with the ability to share on Twitter and Facebook. The technologies used were javascript and jQuery.',
+    demo: 'https://timh1203.github.io/quote-machine/',
+    github: 'https://github.com/timh1203/quote-machine',
+  },
+  travelersChecklist: {
+    name: "Traveler's Checklist",
+    desc:
+      'An app for the travler to make a packing list before a big trip. The technologies used were javascript and jQuery.',
+    demo: 'https://timh1203.github.io/traveler-checklist/',
+    github: 'https://github.com/timh1203/traveler-checklist',
   },
 }
